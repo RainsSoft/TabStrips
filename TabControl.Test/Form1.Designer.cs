8a420039f37923c948e79cc742dccb1490a5ad7b
andyhebear <andyhebear@163.com>
2022-07-04 10:36:20 +0800

    add TabControl
    add TabControl and TabControl.Test project from https://github.com/oozcitak/TabControl

---
+++ TabControl.Test/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿
+namespace TabControl.Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.pagedControl1 = new Manina.Windows.Forms.PagedControl();
+            this.page1 = new Manina.Windows.Forms.Page();
+            this.page2 = new Manina.Windows.Forms.Page();
+            this.page3 = new Manina.Windows.Forms.Page();
+            this.tabControl1 = new Manina.Windows.Forms.TabControl();
+            this.tab1 = new Manina.Windows.Forms.Tab();
+            this.tab2 = new Manina.Windows.Forms.Tab();
+            this.tab3 = new Manina.Windows.Forms.Tab();
+            this.tab4 = new Manina.Windows.Forms.Tab();
+            this.tabControl2 = new Manina.Windows.Forms.TabControl();
+            this.tab5 = new Manina.Windows.Forms.Tab();
+            this.tab6 = new Manina.Windows.Forms.Tab();
+            this.tab7 = new Manina.Windows.Forms.Tab();
+            this.tab8 = new Manina.Windows.Forms.Tab();
+            this.tabControl3 = new Manina.Windows.Forms.TabControl();
+            this.tab10 = new Manina.Windows.Forms.Tab();
+            this.tab11 = new Manina.Windows.Forms.Tab();
+            this.tab12 = new Manina.Windows.Forms.Tab();
+            this.tabControl4 = new Manina.Windows.Forms.TabControl();
+            this.tab9 = new Manina.Windows.Forms.Tab();
+            this.tab13 = new Manina.Windows.Forms.Tab();
+            this.tab14 = new Manina.Windows.Forms.Tab();
+            this.tab15 = new Manina.Windows.Forms.Tab();
+            this.tab16 = new Manina.Windows.Forms.Tab();
+            this.tab17 = new Manina.Windows.Forms.Tab();
+            this.pagedControl1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            this.tabControl3.SuspendLayout();
+            this.tabControl4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pagedControl1
+            // 
+            this.pagedControl1.Controls.Add(this.page1);
+            this.pagedControl1.Controls.Add(this.page2);
+            this.pagedControl1.Controls.Add(this.page3);
+            this.pagedControl1.Location = new System.Drawing.Point(12, 12);
+            this.pagedControl1.Name = "pagedControl1";
+            this.pagedControl1.Size = new System.Drawing.Size(300, 200);
+            this.pagedControl1.TabIndex = 0;
+            // 
+            // page1
+            // 
+            this.page1.Location = new System.Drawing.Point(1, 1);
+            this.page1.Name = "page1";
+            this.page1.Size = new System.Drawing.Size(298, 198);
+            this.page1.Text = "Page 1";
+            // 
+            // page2
+            // 
+            this.page2.Location = new System.Drawing.Point(1, 1);
+            this.page2.Name = "page2";
+            this.page2.Size = new System.Drawing.Size(298, 198);
+            this.page2.Text = "Page 2";
+            // 
+            // page3
+            // 
+            this.page3.Location = new System.Drawing.Point(1, 1);
+            this.page3.Name = "page3";
+            this.page3.Size = new System.Drawing.Size(298, 198);
+            this.page3.Text = "Page 3";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tab1);
+            this.tabControl1.Controls.Add(this.tab4);
+            this.tabControl1.Controls.Add(this.tab2);
+            this.tabControl1.Controls.Add(this.tab3);
+            this.tabControl1.Controls.Add(this.tab14);
+            this.tabControl1.Controls.Add(this.tab15);
+            this.tabControl1.Controls.Add(this.tab16);
+            this.tabControl1.Controls.Add(this.tab17);
+            this.tabControl1.Location = new System.Drawing.Point(332, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Size = new System.Drawing.Size(249, 200);
+            this.tabControl1.TabIndex = 1;
+            this.tabControl1.PageChanged += new System.EventHandler<Manina.Windows.Forms.PageChangedEventArgs>(this.tabControl1_PageChanged);
+            // 
+            // tab1
+            // 
+            this.tab1.Location = new System.Drawing.Point(1, 20);
+            this.tab1.Name = "tab1";
+            this.tab1.Size = new System.Drawing.Size(247, 179);
+            this.tab1.Text = "tab1";
+            // 
+            // tab2
+            // 
+            this.tab2.Location = new System.Drawing.Point(1, 20);
+            this.tab2.Name = "tab2";
+            this.tab2.Size = new System.Drawing.Size(298, 179);
+            this.tab2.Text = "tab2";
+            // 
+            // tab3
+            // 
+            this.tab3.Location = new System.Drawing.Point(1, 20);
+            this.tab3.Name = "tab3";
+            this.tab3.Size = new System.Drawing.Size(298, 179);
+            this.tab3.Text = "tab3";
+            // 
+            // tab4
+            // 
+            this.tab4.Location = new System.Drawing.Point(1, 20);
+            this.tab4.Name = "tab4";
+            this.tab4.Size = new System.Drawing.Size(247, 179);
+            this.tab4.Text = "tab4";
+            this.tab4.Click += new System.EventHandler(this.tab4_Click);
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tab5);
+            this.tabControl2.Controls.Add(this.tab6);
+            this.tabControl2.Controls.Add(this.tab7);
+            this.tabControl2.Controls.Add(this.tab8);
+            this.tabControl2.Location = new System.Drawing.Point(13, 256);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.Size = new System.Drawing.Size(247, 200);
+            this.tabControl2.TabIndex = 2;
+            this.tabControl2.TabLocation = ((Manina.Windows.Forms.TabLocation)((Manina.Windows.Forms.TabLocation.Far | Manina.Windows.Forms.TabLocation.Left)));
+            this.tabControl2.TextDirection = Manina.Windows.Forms.TextDirection.Down;
+            // 
+            // tab5
+            // 
+            this.tab5.Location = new System.Drawing.Point(20, 1);
+            this.tab5.Name = "tab5";
+            this.tab5.Size = new System.Drawing.Size(226, 198);
+            this.tab5.Text = "tab5";
+            // 
+            // tab6
+            // 
+            this.tab6.Location = new System.Drawing.Point(1, 20);
+            this.tab6.Name = "tab6";
+            this.tab6.Size = new System.Drawing.Size(298, 179);
+            this.tab6.Text = "tab6";
+            // 
+            // tab7
+            // 
+            this.tab7.Location = new System.Drawing.Point(37, 1);
+            this.tab7.Name = "tab7";
+            this.tab7.Size = new System.Drawing.Size(262, 198);
+            this.tab7.Text = "tab7";
+            // 
+            // tab8
+            // 
+            this.tab8.Location = new System.Drawing.Point(20, 1);
+            this.tab8.Name = "tab8";
+            this.tab8.Size = new System.Drawing.Size(226, 198);
+            this.tab8.Text = "tab8";
+            // 
+            // tabControl3
+            // 
+            this.tabControl3.Controls.Add(this.tab10);
+            this.tabControl3.Controls.Add(this.tab11);
+            this.tabControl3.Controls.Add(this.tab12);
+            this.tabControl3.Location = new System.Drawing.Point(289, 257);
+            this.tabControl3.Name = "tabControl3";
+            this.tabControl3.Size = new System.Drawing.Size(219, 200);
+            this.tabControl3.TabIndex = 3;
+            this.tabControl3.TabLocation = ((Manina.Windows.Forms.TabLocation)((Manina.Windows.Forms.TabLocation.Center | Manina.Windows.Forms.TabLocation.Right)));
+            this.tabControl3.TextDirection = Manina.Windows.Forms.TextDirection.Up;
+            // 
+            // tab10
+            // 
+            this.tab10.Location = new System.Drawing.Point(1, 1);
+            this.tab10.Name = "tab10";
+            this.tab10.Size = new System.Drawing.Size(198, 198);
+            this.tab10.Text = "tab10";
+            // 
+            // tab11
+            // 
+            this.tab11.Location = new System.Drawing.Point(1, 1);
+            this.tab11.Name = "tab11";
+            this.tab11.Size = new System.Drawing.Size(279, 198);
+            this.tab11.Text = "tab11";
+            // 
+            // tab12
+            // 
+            this.tab12.Location = new System.Drawing.Point(1, 1);
+            this.tab12.Name = "tab12";
+            this.tab12.Size = new System.Drawing.Size(279, 198);
+            this.tab12.Text = "tab12";
+            // 
+            // tabControl4
+            // 
+            this.tabControl4.Controls.Add(this.tab9);
+            this.tabControl4.Controls.Add(this.tab13);
+            this.tabControl4.Location = new System.Drawing.Point(544, 258);
+            this.tabControl4.Name = "tabControl4";
+            this.tabControl4.Size = new System.Drawing.Size(300, 200);
+            this.tabControl4.TabIndex = 4;
+            this.tabControl4.TabLocation = ((Manina.Windows.Forms.TabLocation)((Manina.Windows.Forms.TabLocation.Center | Manina.Windows.Forms.TabLocation.Bottom)));
+            // 
+            // tab9
+            // 
+            this.tab9.Location = new System.Drawing.Point(1, 1);
+            this.tab9.Name = "tab9";
+            this.tab9.Size = new System.Drawing.Size(298, 179);
+            this.tab9.Text = "tab9";
+            // 
+            // tab13
+            // 
+            this.tab13.Location = new System.Drawing.Point(1, 20);
+            this.tab13.Name = "tab13";
+            this.tab13.Size = new System.Drawing.Size(298, 179);
+            this.tab13.Text = "tab13";
+            // 
+            // tab14
+            // 
+            this.tab14.Location = new System.Drawing.Point(1, 20);
+            this.tab14.Name = "tab14";
+            this.tab14.Size = new System.Drawing.Size(247, 179);
+            this.tab14.Text = "tab14";
+            // 
+            // tab15
+            // 
+            this.tab15.Location = new System.Drawing.Point(1, 20);
+            this.tab15.Name = "tab15";
+            this.tab15.Size = new System.Drawing.Size(247, 179);
+            this.tab15.Text = "tab15";
+            // 
+            // tab16
+            // 
+            this.tab16.Location = new System.Drawing.Point(1, 20);
+            this.tab16.Name = "tab16";
+            this.tab16.Size = new System.Drawing.Size(247, 179);
+            this.tab16.Text = "tab16";
+            // 
+            // tab17
+            // 
+            this.tab17.Location = new System.Drawing.Point(1, 20);
+            this.tab17.Name = "tab17";
+            this.tab17.Size = new System.Drawing.Size(247, 179);
+            this.tab17.Text = "tab17";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 506);
+            this.Controls.Add(this.tabControl4);
+            this.Controls.Add(this.tabControl3);
+            this.Controls.Add(this.tabControl2);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.pagedControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pagedControl1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabControl2.ResumeLayout(false);
+            this.tabControl3.ResumeLayout(false);
+            this.tabControl4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Manina.Windows.Forms.PagedControl pagedControl1;
+        private Manina.Windows.Forms.Page page1;
+        private Manina.Windows.Forms.Page page2;
+        private Manina.Windows.Forms.Page page3;
+        private Manina.Windows.Forms.TabControl tabControl1;
+        private Manina.Windows.Forms.Tab tab1;
+        private Manina.Windows.Forms.Tab tab2;
+        private Manina.Windows.Forms.Tab tab3;
+        private Manina.Windows.Forms.Tab tab4;
+        private Manina.Windows.Forms.TabControl tabControl2;
+        private Manina.Windows.Forms.Tab tab5;
+        private Manina.Windows.Forms.Tab tab6;
+        private Manina.Windows.Forms.Tab tab7;
+        private Manina.Windows.Forms.Tab tab8;
+        private Manina.Windows.Forms.TabControl tabControl3;
+        private Manina.Windows.Forms.Tab tab10;
+        private Manina.Windows.Forms.Tab tab11;
+        private Manina.Windows.Forms.Tab tab12;
+        private Manina.Windows.Forms.TabControl tabControl4;
+        private Manina.Windows.Forms.Tab tab9;
+        private Manina.Windows.Forms.Tab tab13;
+        private Manina.Windows.Forms.Tab tab14;
+        private Manina.Windows.Forms.Tab tab15;
+        private Manina.Windows.Forms.Tab tab16;
+        private Manina.Windows.Forms.Tab tab17;
+    }
+}
+
---
+++ TabControl.Test/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TabControl.Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void tab4_Click(object sender, EventArgs e) {
+
+        }
+
+        private void tabControl1_PageChanged(object sender, Manina.Windows.Forms.PageChangedEventArgs e) {
+
+        }
+    }
+}
