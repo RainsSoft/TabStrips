4590c5afa60bef8b6e298757e66832b67f1199a3
andyhebear <andyhebear@163.com>
2022-07-28 16:48:56 +0800

    updaye

--- TabStrip/Enums.cs
+++ TabStrip/Enums.cs
@@ -29,25 +29,5 @@ namespace FarsiLibrary.Win {
         SelectionChanged
     }
     
-  
-    //public class _CONST {
-    
-    //    //public static Color TAB_ACTIVE_TITLE_BACKGROUND_1 = SystemColors.ControlLightLight;
-    //    //public static Color TAB_ACTIVE_TITLE_BACKGROUND_2 = SystemColors.Window;
-    //    //public static Color TAB_ACTIVE_TOOLBAR_BACKGROUND = SystemColors.Window;
-
-    //    public static Color TAB_ACTIVE_TITLE_BACKGROUND_1 = Color.White;
-    //    public static Color TAB_ACTIVE_TITLE_BACKGROUND_2 = Color.LightSkyBlue;
-    //    public static Color TAB_ACTIVE_TOOLBAR_BACKGROUND = SystemColors.Control;//Color.Red;
-    //    public static Color TAB_ACTIVE_BORDER_COLOR = Color.FromArgb(127, 157, 185);// Color.Gray;//, Color.FromArgb(164, 185, 127), Color.FromArgb(165, 172, 178), Color.FromArgb(132, 130, 132)
-    //    //private static Pen _Pen_TAB_ACTIVE_BORDER_COLOR;
-    //    //public static Pen Pen_TAB_ACTIVE_BORDER_COLOR {
-    //    //    get {
-    //    //        if(_Pen_TAB_ACTIVE_BORDER_COLOR == null) {
-    //    //            _Pen_TAB_ACTIVE_BORDER_COLOR = new Pen(TAB_ACTIVE_BORDER_COLOR);
-    //    //        }
-    //    //        return _Pen_TAB_ACTIVE_BORDER_COLOR;
-    //    //    }
-    //    //}
-    //}
+ 
 }
--- TabStrip/_CONST.cs
+++ TabStrip/_CONST.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Text;
+﻿using System.Drawing;
+namespace FarsiLibrary.Win {
 
-namespace FarsiLibrary.Win
-{
-    public class _CONST
-    {
-        //public static Color TAB_ACTIVE_TITLE_BACKGROUND_1 = Color.LightGray;
-        ////public static Color TAB_ACTIVE_TITLE_BACKGROUND_2 = Color.WhiteSmoke;
-        //public static Color TAB_ACTIVE_TOOLBAR_BACKGROUND = Color.WhiteSmoke;
+    public class _CONST {
 
         //public static Color TAB_ACTIVE_TITLE_BACKGROUND_1 = SystemColors.ControlLightLight;
         //public static Color TAB_ACTIVE_TITLE_BACKGROUND_2 = SystemColors.Window;
@@ -19,6 +11,14 @@ namespace FarsiLibrary.Win
         public static Color TAB_ACTIVE_TITLE_BACKGROUND_2 = Color.LightSkyBlue;
         public static Color TAB_ACTIVE_TOOLBAR_BACKGROUND = SystemColors.Control;//Color.Red;
         public static Color TAB_ACTIVE_BORDER_COLOR = Color.FromArgb(127, 157, 185);// Color.Gray;//, Color.FromArgb(164, 185, 127), Color.FromArgb(165, 172, 178), Color.FromArgb(132, 130, 132)
-
+        //private static Pen _Pen_TAB_ACTIVE_BORDER_COLOR;
+        //public static Pen Pen_TAB_ACTIVE_BORDER_COLOR {
+        //    get {
+        //        if(_Pen_TAB_ACTIVE_BORDER_COLOR == null) {
+        //            _Pen_TAB_ACTIVE_BORDER_COLOR = new Pen(TAB_ACTIVE_BORDER_COLOR);
+        //        }
+        //        return _Pen_TAB_ACTIVE_BORDER_COLOR;
+        //    }
+        //}
     }
 }
